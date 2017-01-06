6ba1cbc9e28fb327cf4bf32b9e16ab78f0626607
Xu <I068191@global.corp.sap>
2017-01-06 09:27:50 +0800

    Removed servlet annotation for demo

--- src/main/java/com/sap/dbs/dbx/i068191/SpringbootOlingoApplication.java
+++ src/main/java/com/sap/dbs/dbx/i068191/SpringbootOlingoApplication.java
@@ -24,8 +24,8 @@ import com.sap.dbs.dbx.i068191.servlet.SimpleODataServlet;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootApplication
-@ServletComponentScan
+//@SpringBootApplication
+//@ServletComponentScan
 @Slf4j
 public class SpringbootOlingoApplication {
 
@@ -78,19 +78,19 @@ public class SpringbootOlingoApplication {
 //		return adapter;
 //	}
 	
-	@Bean(name="MyODataServiceFactory")
-	public ODataServiceFactory getServiceFactory(){
-		return new MyODataServiceFactory("com.sap.dbs.dbx.i068191.demo");
-	}
-
-	@Bean(name="com.sap.dbs.dbx.i068191.demo.BookODataAgent")
-	public BookODataAgent personODataAgent(){
-		log.info("return BookODataAgent object");
-		return new BookODataAgent();
-	}
+//	@Bean(name="MyODataServiceFactory")
+//	public ODataServiceFactory getServiceFactory(){
+//		return new MyODataServiceFactory("com.sap.dbs.dbx.i068191.demo");
+//	}
+//
+//	@Bean(name="com.sap.dbs.dbx.i068191.demo.BookODataAgent")
+//	public BookODataAgent personODataAgent(){
+//		log.info("return BookODataAgent object");
+//		return new BookODataAgent();
+//	}
 }
 
-@WebServlet(urlPatterns = { "/odata.svc/*" })
+//@WebServlet(urlPatterns = { "/odata.svc/*" })
 class DemoODataServlet extends SimpleODataServlet {
 
 	/**
